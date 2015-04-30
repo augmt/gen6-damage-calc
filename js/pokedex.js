bc92ac4f3332611cd31a159299d22c230cbb9e88
gamut-was-taken <gamut-was-taken@users.noreply.github.com>
2015-04-29 22:42:24 -0400

    Correct Mega-Sceptile's ability to `Lightning Rod`

--- js/pokedex.js
+++ js/pokedex.js
@@ -10475,7 +10475,7 @@ var POKEDEX_XY = $.extend(true, {}, POKEDEX_BW, {
       "sp": 145
     },
     "w": 55.2,
-    "ab": "Lightningrod",
+    "ab": "Lightning Rod",
     "isAlternateForme": true
   },
   "Mega Scizor": {
